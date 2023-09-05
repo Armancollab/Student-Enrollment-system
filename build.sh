a52bb72f23173ae8156f450b56fe4bcad8ae3f14
Arman Mustamandyar <109197285+Armancollab@users.noreply.github.com>
2023-09-05 06:49:36 +0430

    Initial upload of the file

---
+++ build.sh
@@ -0,0 +1,11 @@
+#Build the Project
+
+echo "Building the Project..."
+python3.8 -m pip install -r requirements.txt
+
+echo "Making Migration"
+python3.8 manage.py makemigrations --noinput
+python3.8 manage.py migrate --noinput
+
+echo "Collecting Static..."
+python3.8 manage.py collectstatic --noinput --clear
